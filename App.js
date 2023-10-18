5ac5ad0d57a07d56739018e59ae96a2eb8f49b6c
ViPhong <viphongly2804@gmail.com>
2023-10-18 11:32:53 +0700

    Try CSS: PASSED, Add Button: PASSED, Add onPress functionality: PASSED, add Alert msg: PASSED

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Here's another text</Text>
+      <Text>Also another</Text>
+      <Button title="Request Permission" onPress={sayHello}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const sayHello = () => {
+  alert("Hello there! How are you?")
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffc',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
 });
