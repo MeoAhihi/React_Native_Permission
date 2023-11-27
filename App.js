2e92ec2fb048331ef26d91c94f725d778a72f44b
ViPhong <viphongly2804@gmail.com>
2023-11-27 15:05:34 +0700

    done

--- App.js
+++ App.js
@@ -1,37 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, PermissionsAndroid, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  PermissionsAndroid,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app</Text>
       <Text>Here's another text</Text>
       <Text>Also another</Text>
-      <Button title="Request Permission" onPress={requestCameraPermission}/>
+      <Button title="Request SCAN" onPress={requestBLEscanPermission} />
+      <Button title="Request ADVERTISE" onPress={requestBLEadvPermission} />
+      <Button title="Request CONNECT" onPress={requestBLEconPermission} />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const requestCameraPermission = async () => {
+const requestLocationPermission = async () => {
   try {
     const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.CAMERA,
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
       {
-        title: "Can I open the camera for fun?",
-        message: "It is for testing the code, plzzzzz"
+        title: "Can I open the get location for fun?",
+        message: "It is for testing the code, plzzzzz",
+        buttonPositive: "OK",
+        buttonNeutral: "Maybe later",
+        buttonNegative: "Cancel",
       }
-    )
+    );
+    console.log(
+      granted
+    );
   } catch (error) {
-    
-  }
-}
+    console.error(error);
+    console.log(
+      granted
+    );
+  } 
+};
+const requestBLEscanPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: "Can I open the scan blueetooth for fun?",
+        message: "It is for testing the code, plzzzzz",
+        buttonPositive: "OK",
+        buttonNeutral: "Maybe later",
+        buttonNegative: "Cancel",
+      }
+    );
+    console.log(
+      granted
+    );
+  } catch (error) {
+    console.error(error);
+    console.log(
+      granted
+    );
+  } 
+};
+const requestBLEadvPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_ADVERTISE,
+      {
+        title: "Can I open the scan blueetooth for fun?",
+        message: "It is for testing the code, plzzzzz",
+        buttonPositive: "OK",
+        buttonNeutral: "Maybe later",
+        buttonNegative: "Cancel",
+      }
+    );
+    console.log(
+      granted
+    );
+  } catch (error) {
+    console.error(error);
+    console.log(
+      granted
+    );
+  } 
+};
+const requestBLEconPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Can I open the scan blueetooth for fun?",
+        message: "It is for testing the code, plzzzzz",
+        buttonPositive: "OK",
+        buttonNeutral: "Maybe later",
+        buttonNegative: "Cancel",
+      }
+    );
+    console.log(
+      granted
+    );
+  } catch (error) {
+    console.error(error);
+    console.log(
+      granted
+    );
+  } 
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffc',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "space-evenly",
   },
 });
